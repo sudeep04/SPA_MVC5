ad486c8449696cc44c4323f4c34144d32e6176b3
Sudeep Reddy <sudeep04@gmail.com>
2016-06-01 16:38:08 +0530

    Added german/English translations

--- SPA_MVC 5/Scripts/app/app.js
+++ SPA_MVC 5/Scripts/app/app.js
@@ -1,4 +1,73 @@
-﻿var module = angular.module("StudentApp", ["ngRoute", "stucontrollers", "sturesources"]);
+﻿var module = angular.module("StudentApp", ["ngRoute", "stucontrollers", "sturesources", "pascalprecht.translate"]);
+
+var translationsEN = {
+    HEADLINE: 'What an awesome module!',
+    PARAGRAPH: 'Srsly!',
+    NAMESPACE: {
+        PARAGRAPH: 'And it comes with awesome features!'
+    },
+    ID: 'Id',
+    FIRSTNAME: 'FirstName',
+    LASTNAME: 'LastName',
+    AGE:'Age',
+    GENDER: 'Gender',
+    ACTION: 'Action',
+    DELETE: 'Delete',
+    FNAME: 'First Name',
+    FNAME_ERROR: 'Student FirstName is required',
+    LNAME: 'Last Name',
+    LNAME_ERROR: 'Provide Last Name for student.',
+    AGE_ERROR_REQD: 'Age is required.',
+    AGE_ERROR_POSITIVE: 'Age must be a positive number.',
+    ADDSTU: 'Add Student',
+    BUTTON_LANG_DE: 'German',
+    BUTTON_LANG_EN: 'English',
+    SEARCH_FIRSTNAME: 'Search by First Name',
+    SEARCH_LASTNAME: 'Search by Last Name',
+    ORDER_BY: 'Order by : ',
+    ASC: 'ascending',
+    DSC: 'descending',
+    MALE: 'Male',
+    FEMALE: 'Female',
+    OTHER: 'Other',
+    Male: 'Male',
+    Female: 'Female',
+    Other: 'Other',
+    HOME:'Home'
+
+};
+
+var translationsDE = {
+    ID: 'Ich würde',
+    FIRSTNAME: 'Vorname',
+    LASTNAME: 'Nachname',
+    AGE: 'Alter',
+    GENDER: 'Geschlecht',
+    ACTION: 'Aktion',
+    DELETE: 'Löschen',
+    FNAME: 'Vorname',
+    FNAME_ERROR: 'Student Vorname ist erforderlich',
+    LNAME: 'Nachname',
+    LNAME_ERROR: 'Geben Sie Nachname für Schüler.',
+    AGE_ERROR_REQD: 'Alter ist nicht erforderlich.',
+    AGE_ERROR_POSITIVE: 'Alter muss eine positive Zahl sein.',
+    ADDSTU: 'In Studenten',
+    BUTTON_LANG_DE: 'Deutsch',
+    BUTTON_LANG_EN: 'Englisch',
+    SEARCH_FIRSTNAME: 'Suche nach Vorname',
+    SEARCH_LASTNAME: 'Suche nach Nachnamen',
+    ORDER_BY: ' Sortieren nach : ',
+    ASC: 'aufsteigend',
+    DSC: 'absteigend',
+    MALE: 'Männlich',
+    FEMALE: 'Weiblich',
+    OTHER: 'Andere',
+    Male: 'Männlich',
+    Female: 'Weiblich',
+    Other: 'Andere',
+    HOME:'xxx'
+
+};
 
 module.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
@@ -17,4 +86,11 @@ module.config(['$routeProvider', function ($routeProvider) {
     otherwise({
         redirectTo: '/home'
     });
+}]);
+
+module.config(['$translateProvider', function ($translateProvider) {
+    // add translation table
+    $translateProvider.translations('en', translationsEN);
+    $translateProvider.translations('de', translationsDE);
+    $translateProvider.preferredLanguage('en');
 }]);
--- SPA_MVC 5/Scripts/app/stucontrollers.js
+++ SPA_MVC 5/Scripts/app/stucontrollers.js
@@ -1,20 +1,27 @@
 ﻿var stucontrollers = angular.module("stucontrollers", []);
 stucontrollers.controller("GetStudentsList",
 
-    function GetStudentsList($scope, $http) {
-    $http.get("/api/students").success(function (data) {
-        $scope.students = data;
-    })
+    function GetStudentsList($scope, $http, $translate) {
+        $http.get("/api/students").success(function (data) {
+            $scope.students = data;
+        });
+        $scope.changeLanguage = function (langKey) {
+            $translate.use(langKey);
+        };
 });
 
-stucontrollers.controller("AddStudent", function ($scope, $http, $location) {
+stucontrollers.controller("AddStudent", function ($scope, $http, $location, $translate)
+{
+    $scope.changeLanguage = function (langKey) {
+        $translate.use(langKey);
+    };
     $scope.student = {
         "FirstName": "",
         "LastName": "",
         "Age": "",
         "Gender": "Male"
     };
-    $scope.genders = ['Male', 'Female', 'Other'];
+    $scope.genders = ['MALE', 'FEMALE', 'OTHER'];
     $scope.pattern = /^\s*\d+\s*$/;
     $scope.Add = function () {
         $http({ method: "POST", data: $scope.student, url: "/api/students" }).
@@ -27,13 +34,17 @@ stucontrollers.controller("AddStudent", function ($scope, $http, $location) {
                 "Gender": ""
             };
         });
+
         alert("Student added to DB");
         $location.path('/allstu');
     }
 });
 
-stucontrollers.controller("AllStudents", function ($scope, $http) {
+stucontrollers.controller("AllStudents", function ($scope, $http,$translate) {
 
+    $scope.changeLanguage = function (langKey) {
+        $translate.use(langKey);
+    };
     $scope.getAll = function () {
         $http({ method: "GET", url: "/api/students" }).
         success(function (data, status, headers, config) {
